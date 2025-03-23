4410db03c6618092979c0f73bc622763ef1bc975
aboodalmsri <1mraboo0de1@gmail.com>
2025-03-23 15:56:57 +0000

    Abood - admin can change profile info

---
+++ Holoul/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Holoul.Models
+{
+    public class ProfileViewModel
+    {
+        [Required(ErrorMessage = "الاسم الأول مطلوب")]
+        [Display(Name = "الاسم الأول")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "الاسم الأخير مطلوب")]
+        [Display(Name = "الاسم الأخير")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
